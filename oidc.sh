abc5f7c97404fd2387cfe810a6526c7e161ccfec
Nigel Leck <175672509+Nigel-ha@users.noreply.github.com>
2024-09-02 13:43:08 +1000

    Update oidc.sh

--- oidc.sh
+++ oidc.sh
@@ -1,1 +1,9 @@
 aws eks describe-cluster --name <cluster-name> --query "cluster.identity.oidc.issuer" --output text
+
+#https://oidc.eks.<region>.amazonaws.com/id/<eks-cluster-id>
+
+aws iam list-open-id-connect-providers | grep <eks-cluster-id>
+aws iam create-open-id-connect-provider \
+  --url https://oidc.eks.<region>.amazonaws.com/id/<eks-cluster-id> \
+  --client-id-list sts.amazonaws.com \
+  --thumbprint-list <thumbprint>
